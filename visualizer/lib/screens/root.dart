a31fe45785605439fc1fec1182fa7b9376049542
Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>
2021-07-27 14:59:03 -0300

    Migrate to Flutter project

---
+++ visualizer/lib/screens/root.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Root extends StatelessWidget {
+  const Root({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
